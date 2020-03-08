8c8ea8b716f923636537c509baa647ef539523bc
jhyoo9161 <jhyoo9161@gmail.com>
2020-03-09 00:07:20 +0900

    미들웨어 및 모듈 설치

--- server/main.js
+++ server/main.js
@@ -1,14 +1,32 @@
 import express from 'express';
 import path from 'path';
-
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
-    
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import session from 'express-session';
+
 const app = express();
 const port = 3000;
 const devPort = 8080;
 
 app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// mongo db connection
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+mongoose.connect('mongodb://localhost/jihoYoo');
+
+// use session
+app.use(session({
+    secret : 'jiho1q2w3e!@',
+    resave : false,
+    saveUninitialized : true
+}));
 
 app.get('/main', (req, res) => {
     return res.send('Hello World');
