6b8c4608be615725fd07a99110ec3eeed8aebf43
jhyoo9161 <jhyoo9161@gmail.com>
2020-05-01 18:04:09 +0900

    메모 쓰기 구현

--- server/routes/memo.js
+++ server/routes/memo.js
@@ -4,9 +4,37 @@ import mongoose from 'mongoose';
 
 const router = express.Router();
 
-// WRITE MEMO
+/* 
+    WRITE MEMO: POST /api/memo
+    BODY SAMPLE: { contents: "sample "}
+    ERROR CODES
+        1: NOT LOGGED IN
+        2: EMPTY CONTENTS
+*/
 router.post('/', (req, res) => {
+    if (typeof req.session.loginInfo === 'undefined') {
+        return res.status(403).json({
+            error : "NOT LOGGED IN",
+            code : 1
+        });
+    }
 
+    if (typeof req.body.contents !== 'string' || req.body.contents === '') {
+        return res.status(400).json({
+            error : "EMPTY CONTENTS",
+            code : 2
+        });
+    }
+    
+    let memo = new Memo({
+        writer : req.session.loginInfo.username,
+        contents : req.body.contents
+    });
+
+    memo.save(err => {
+        if (err) throw err;
+        return res.json({ sucess : true });
+    })
 });
 
 // MODIFY MEMO
